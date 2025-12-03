eb8826aa2a539011473dee23482f869713ec34c5
jonathan.owen <jonathan.owen@students.snow.edu>
2025-12-03 10:22:06 -0700

    Implemented Dijkstra's method

--- DSA Final/Map.cs
+++ DSA Final/Map.cs
@@ -18,38 +18,99 @@ namespace DSA_Final
             Graph = new Dictionary<string, Dictionary<string, int>>();
         }
 
-        public void AddVertex(string vertex)
+        public void AddNode(string node)
         {
-            if(!Graph.ContainsKey(vertex))
+            if(!Graph.ContainsKey(node))
             {
-                Graph[vertex] = new Dictionary<string, int>();
+                Graph[node] = new Dictionary<string, int>();
             }
         }
 
-        public void AddEdge(string firstVertex, string secondVertex, int weight)
+        public void AddEdge(string firstNode, string secondNode, int weight)
         {
             if(weight < 0)
             {
-                throw new ArgumentException("Cannot be below 0");
+                weight = 0;
             }
 
-            AddVertex(firstVertex);
-            AddVertex(secondVertex);
-            Graph[firstVertex][secondVertex] = weight;
+            AddNode(firstNode);
+            AddNode(secondNode);
+            Graph[firstNode][secondNode] = weight;
         }
 
         public void PrintGraph()
         {
-            foreach(var vertex in Graph)
+            foreach(var node in Graph)
             {
-                Console.Write($"{vertex.Key} goes to: ");
-                foreach(var value in vertex.Value)
+                Console.Write($"{node.Key} goes to: ");
+                foreach(var value in node.Value)
                 {
-                    Console.Write($"{value.Key} {value.Value}");
+                    Console.Write($"{value.Key} with a distance of {value.Value}, ");
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        public Dictionary<string, int> Dijkstra(string start)
+        {
+
+
+            // Step 1: Create a dictionary to store shortest distances
+            var distances = new Dictionary<string, int>();
+
+            // Step 2: Initialize all distances to "infinity" (here, int.MaxValue)
+            foreach (var node in Graph.Keys)
+            {
+                distances[node] = int.MaxValue;
+            }
+
+            // Distance to the start node is always 0
+            distances[start] = 0;
+
+            // Step 3: Keep track of visited nodes
+            var visited = new HashSet<string>();
+
+            // Step 4: Loop until all nodes are visited
+            while (visited.Count < Graph.Count)
+            {
+                // Find the unvisited node with the smallest distance
+                string currentNode = null;
+                int smallestDistance = int.MaxValue;
+
+                foreach (var node in Graph.Keys)
+                {
+                    if (!visited.Contains(node) && distances[node] < smallestDistance)
+                    {
+                        smallestDistance = distances[node];
+                        currentNode = node;
+                    }
+                }
+
+                // If we can't find a reachable node, break
+                if (currentNode == null)
+                {
+                    break;
+                }
+
+                // Mark this node as visited
+                visited.Add(currentNode);
+
+                // Step 5: Update distances to neighbors
+                foreach (var neighbor in Graph[currentNode])
+                {
+                    int newDistance = distances[currentNode] + neighbor.Value;
+
+                    // If new path is shorter, update it
+                    if (newDistance < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = newDistance;
+                    }
+                }
+            }
+
+            // Return the dictionary of shortest distances
+            return distances;
         }
     }
 }
